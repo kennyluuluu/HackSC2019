ff2d8bcd6ab71b9ac36d2a8a304f6fdadf9e8c3a
Kenny Luu <kluu@cs130.org>
2019-04-13 03:29:35 -0700

    retrieve department and dates

--- src/Retrieve.java
+++ src/Retrieve.java
@@ -5,21 +5,78 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import java.util.Date;
+import java.util.Calender;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class Retrieve {
+	public static int getYear() {
+		Date today = new Date();
+		Calender cal = Calender.getInstance();
+		cal.setTime(today);
+		return cal.get(Calender.YEAR);
+	}
+
+	public static int getSemester() {
+		Date today = new Date();
+		Calender cal = Calender.getInstance();
+		cal.setTime(today);
+		int month = cal.get(Calender.MONTH);
+		if (month > 0 && month <= 5)
+			return 1;
+		else if (month > 5 && month <= 7)
+			return 2;
+		else return 3;	
+	}
+
+	public static <T> Set<T> mergeSet(Set<T> a, Set<T> b)
+	{
+		return new HashSet<T>() {{
+				addAll(a);
+				addAll(b);
+		}};
+	}
+
+	public static HashSet<String> findDepartmentCodes(JsonElement x) {
+		HashSet<String> codes = new HashSet<String>()
+		if (x.isJsonArray()) {
+			for (int i = 0; i < x.size(); i++) {
+				codes = mergeSet(codes, findDepartmentCodes(x.getJsonObject(i));
+			}
+		}
+		else if (x.isJsonObject()) {
+			for (int j = 0; j < x.size(); j++) {
+				if (x.has("code") {
+					codes.add(x.getJsonObject("code").getAsString());
+				}
+				codes = mergeSet(codes, r(x.getJsonObject(i));
+			}
+		}
+		return codes;
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		JsonObject rootObj = readJsonFromURL("https://web-app.usc.edu/web/soc/api/departments/20143");
-		JsonArray schools = rootObj.get("department").getAsJsonArray();
-		String jsonString = schools.toString();
-		System.out.println(jsonString);
-		System.out.println(jsonString.matches("\\\"code\":\"([A-Z])\\"));
+		HashSet<String> codes = new HashSet<String>();
+		codes = r(rootObj);
+		int year = getYear();
+		// 1 is sprint, 2 is summer, 3 is fall
+		int semester = getSemester();
+
+		for(int i = 0; i < codes.size(); i++)
+			JsonObject classJson = readJsonFromURL("https://web-app.usc.edu/web/soc/api/classes/" + codes[i] + "/" + Integer.toString(year) + Integer.toString(semester));
+			// RICHARD's part
+		}
+
+
+		//System.out.println(jsonString);
+		//System.out.println(jsonString.matches("\\\"code\":\"([A-Z])\\"));
 		
 	}
 	
